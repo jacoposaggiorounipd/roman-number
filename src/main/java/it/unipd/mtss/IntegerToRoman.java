3e0bd26f862fb601a42b7c2372f10dd912125fed
jacoposaggiorounipd <jacopo.saggioro@studenti.unipd.it>
2026-05-08 12:29:40 +0200

    Implementazione Integer to Roman primi 3, close #4

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,7 +7,24 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        // TODO
-        return null;
+        if (number < 1 || number > 3) {
+            throw new IllegalArgumentException(
+                "Valore numerico non compreso tra 1 e 3");
+        }
+
+        int[] values = {1};
+
+        String[] romanSymbols = {"I"};
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                result.append(romanSymbols[i]);
+                number -= values[i];
+            }
+        }
+        
+        return result.toString();
     }
 }
