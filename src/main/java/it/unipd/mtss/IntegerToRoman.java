fec4dab8709b5eb71f83de531f24fd725a95282a
jacoposaggiorounipd <jacopo.saggioro@studenti.unipd.it>
2026-05-08 13:43:03 +0200

    Implementazione Integer to Roman primi 10-20, close #6, close #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,14 +7,14 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        if (number < 1 || number > 6) {
+        if (number < 1 || number > 20) {
             throw new IllegalArgumentException(
-                "Valore numerico non compreso tra 1 e 6");
+                "Valore numerico non compreso tra 1 e 20");
         }
 
-        int[] values = {5, 4, 1};
+        int[] values = {10, 9, 5, 4, 1};
 
-        String[] romanSymbols = {"V", "IV", "I"};
+        String[] romanSymbols = {"X", "IX", "V", "IV", "I"};
 
         StringBuilder result = new StringBuilder();
 
